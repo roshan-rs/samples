acc1693ed5bafb6e63c9eaf75a92a98b33f9608e
roshan-rs <81679452+roshan-rs@users.noreply.github.com>
2021-07-12 23:03:11 +0530

    new changes

--- HelloFX/CLI/hellofx/HelloFX.java
+++ HelloFX/CLI/hellofx/HelloFX.java
@@ -1,4 +1,4 @@
-import javafx.application.Application;
+//import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
